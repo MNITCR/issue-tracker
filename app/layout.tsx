24c9e085bee847bfb3846b100712c3c5a90c4ce1
MNITCR <108656979+MNITCR@users.noreply.github.com>
2023-11-07 15:06:53 +0700

    cm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import '@radix-ui/themes/styles.css';
+import { Theme ,ThemePanel} from '@radix-ui/themes';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './Navbar';
@@ -18,8 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
+        <Theme appearance="light" accentColor="iris" grayColor="sand" panelBackground="solid">
+          <Navbar />
+          <main className='p-5'>{children}</main>
+          {/* <ThemePanel /> */}
+        </Theme>
       </body>
     </html>
   )
